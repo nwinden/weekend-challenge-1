41d6c54b4930a64974bd7d84f16297462e3e371c
nwinden <noah.winden@gmail.com>
2016-04-30 14:14:44 -0500

    added 'add', 'delete', and dynamic montly costs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
   var array = [];
 
+  var monthlySal = 0;
+
   $('.employeeData').on('submit', function(event) {
 
     event.preventDefault();
@@ -9,7 +11,9 @@ $(document).ready(function() {
     var values = {};
 
     $.each($('.employeeData').serializeArray(), function(i, field) {
+
       values[field.name] = field.value;
+
     });
 
     // clear out inputs
@@ -21,20 +25,33 @@ $(document).ready(function() {
     // append to DOM
     appendDom(values);
 
+    monthlySal += Math.round(values.employeeSalary / 12);
+
+    $('.monTotal').text('Monthly Cost: $' + monthlySal);
+
     console.log(values);
 
   });
 
   function appendDom(empInfo) {
-    $('main').append('<div></div>');
-    var $el = $('main').children().last();
 
-    $el.append('<ul><li><p>First Name: ' + empInfo.employeeFirstName + '</p></li>'
+    $('section').append('<div></div>');
+    var $el = $('section').children().last();
+    $el.append('<div class="employee"><ul><li><p>First Name: ' + empInfo.employeeFirstName + '</p></li>'
                + '<li><p>Last Name: ' + empInfo.employeeLastName + '</p></li>'
                + '<li><p>Employee ID Number: ' + empInfo.employeeNumber + '</p></li>'
                + '<li><p>Job Title: ' + empInfo.employeeJobTitle + '</p></li>'
-               + '<li><p>Salary: $' + empInfo.employeeSalary + '</p></li></ul>');
+               + '<li><p class="salary">Salary: $' + empInfo.employeeSalary + '</p></li></ul>'
+               + '<button type="button" name="button" class="delBut">Delete Employee</button></div>');
+
   }
 
+  $('main').on('click', '.delBut', function(){
+        $(this).parent().remove();
+        var salary = $(this).closest('.employee').find('.salary').text().substring(9);
+        monthlySal -= Math.round(salary / 12);
+        $('.monTotal').text('Monthly Cost: $' + monthlySal);
+
+  });
 
 });
